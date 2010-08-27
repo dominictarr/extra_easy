01afc3647af9e0b3fd10e4edadeda6f594a635e0
Dominic Tarr <dominic.tarr@gmail.com>
2010-08-27 15:00:28 +1200

    push to heroku

--- sinatra_hello.rb
+++ sinatra_hello.rb
@@ -3,7 +3,9 @@ require 'sinatra'
 class HelloSinatra < Sinatra::Base
 
 get '/' do
-  "Hello World FROM SINATRA!"
+  "Hello World FROM SINATRA!" +
+  "\n\n" + 
+  ENV['DATABASE_URL']
 end
 get '/meet' do
   "Hello #{params[:name]}!"
