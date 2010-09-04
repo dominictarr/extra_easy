1e2c57bda9a7f1c2924978a1002d63345b766294
Dominic Tarr <dominic.tarr@gmail.com>
2010-09-04 19:25:07 +1200

    deploying to heroku

--- coupler.rb
+++ coupler.rb
@@ -4,7 +4,7 @@ require 'yaml'
 module Coupler
 
 	def couple(test)
-		url = URI.parse("http://localhost:5678/couple/#{test.to_s}")
+		url = URI.parse("http://meta-modular.com/couple/#{test.to_s}")
 		ary = YAML::load(Net::HTTP.get(url))
 		eval ary[1] #require code
 		eval ary[0].to_s #init class
